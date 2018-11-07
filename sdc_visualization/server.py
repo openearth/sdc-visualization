36abdcf1cc8f8d9dbbb4707be705381eea4f11c2
Fedor Baart <fedor.baart@deltares.nl>
2018-11-07 15:48:03 +0100

    server implementation

--- sdc_visualization/server.py
+++ sdc_visualization/server.py
@@ -1,10 +1,14 @@
 """Create a visualization server."""
 
+import datetime
+
+import netCDF4
+import numpy as np
+import geojson
+
 from flask import Blueprint, Flask, jsonify, current_app, request
 from flask_cors import CORS
 
-
-import datetime
 blueprint = Blueprint('public', __name__, static_folder='../static')
 
 
@@ -27,8 +31,87 @@ def dataset_slice():
     year = int(request.values.get('year', datetime.datetime.now().year))
     print(request.form)
     depth = int(request.values.get('depth', 0))
-    resp = [year, depth]
-    return jsonify(resp)
+
+
+    """
+    read some variables and return an open file handle,
+    based on data selection.
+    """
+    data = current_app.ds
+
+    # TODO: slicing in time!
+    t0 = netCDF4.date2num(
+        datetime.datetime(year=year, month=1, day=1),
+        data.variables['date_time'].units
+    )
+    t1 = netCDF4.date2num(
+        datetime.datetime(year=year + 1, month=1, day=1),
+        data.variables['date_time'].units
+    )
+
+    # ensure that our array is always masked
+    date_time = np.ma.masked_array(
+        data.variables['date_time'][:]
+    )
+    is_in_date = np.logical_and(
+        date_time[:] >= t0,
+        date_time[:] < t1
+    ).data
+    t = np.empty(
+        len(date_time[is_in_date]),
+        dtype=type(datetime.datetime.now())
+    )
+
+
+    # split nans and notnans makes it much faster
+    dtf = np.where(date_time[is_in_date].mask == False)
+    dtt = np.where(date_time[is_in_date].mask == True)
+    t[dtf] = netCDF4.num2date(
+        date_time[is_in_date][dtf],
+        data.variables['date_time'].units
+    )
+    # do we have any masked values
+    if dtt[0]:
+        t[dtt] = netCDF4.num2date(
+            date_time[is_in_date][dtt],
+            data.variables['date_time'].units
+        )
+
+    # # TODO: slicing through Depth... Hard with this sort of unstructured netcdf.
+    # if data['var1'].long_name == "Depth":
+    #     depth = None
+    # else:
+    depth = None
+
+    if 'lat' in data.variables:
+        lat = data['lat'][is_in_date]
+    elif 'latitude' in data.variables:
+        lat = data['latitude'][is_in_date]
+    if 'lon' in data.variables:
+        lon = data['lon'][is_in_date]
+    elif 'longitude' in data.variables:
+        lon = data['longitude'][is_in_date]
+
+    resp = {
+        "grids": {
+            "lat": lat,
+            "lon": lon,
+            "time": t,
+            "depth": depth,
+            "nc_file": data
+        },
+        "trajectories": [],
+        "profiles": []
+
+    }
+    geometry = geojson.MultiPoint(
+        np.c_[lon, lat].tolist()
+    )
+    feature = geojson.Feature(
+        geometry=geometry,
+        properties={}
+    )
+    return jsonify(feature)
 
 def create_app(ds):
     """Create an app."""
