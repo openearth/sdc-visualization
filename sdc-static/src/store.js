7bbed59d66e3fd2f782ada2bc6bf6bc81902e252
Fedor Baart <fedor.baart@deltares.nl>
2019-04-10 15:50:10 +0200

    styles and state

--- sdc-static/src/store.js
+++ sdc-static/src/store.js
@@ -4,13 +4,17 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    serverUrl: "http://localhost:5000"
-  },
-  mutations: {
+    state: {
+        serverUrl: "http://localhost:5000",
+        credentials: null
+    },
+    mutations: {
+        credentials (state, credentials) {
+            Vue.set(state, 'credentials', credentials)
+        }
 
-  },
-  actions: {
+    },
+    actions: {
 
-  }
+    }
 })
