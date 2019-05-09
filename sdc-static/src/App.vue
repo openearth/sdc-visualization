0ad677bca3b5fe1d4fbffb34b63407790d7f294b
Fedor Baart <fedor.baart@deltares.nl>
2019-05-09 15:03:16 +0100

    use vuetify

--- sdc-static/src/App.vue
+++ sdc-static/src/App.vue
@@ -1,15 +1,14 @@
 <template>
 <div id="app" >
-    <router-view></router-view>
+    <v-app>
+        <router-view></router-view>
+    </v-app>
+
 </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(BootstrapVue)
 export default {
     name: 'App',
     components: {
