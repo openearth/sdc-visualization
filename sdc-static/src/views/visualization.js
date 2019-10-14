f84c0c1e0c6b31d43e7fa856393b8f5d294e0641
Fedor Baart <fedor.baart@deltares.nl>
2019-10-14 17:26:12 +0200

    implement multiple datasets

--- sdc-static/src/views/visualization.js
+++ sdc-static/src/views/visualization.js
@@ -1,12 +1,12 @@
-import { mapactions, mapstate } from 'vuex'
+import { mapActions, mapState } from 'vuex'
 
 // todo: use proper date formatting
 // import moment from 'moment'
 import _ from 'lodash'
 
-import timeslider from '@/components/TimeSlider'
-import depthslider from '@/components/DepthSlider'
-import chartcomponent from '@/components/ChartComponent'
+import timeSlider from '@/components/TimeSlider'
+import depthSlider from '@/components/DepthSlider'
+import chartComponent from '@/components/ChartComponent'
 import store from '@/store.js'
 
 import layers from './ts-layers.json'
@@ -18,22 +18,22 @@ export default {
     store,
     name: 'visualization',
     components: {
-        "v-time-slider": timeslider,
-        "v-depth-slider": depthslider,
-        "chart-component": chartcomponent
+        "v-time-slider": timeSlider,
+        "v-depth-slider": depthSlider,
+        "chart-component": chartComponent
     },
     data () {
 
         return {
-            menudrawer: false,
-            plotdrawer: true,
+            menuDrawer: false,
+            plotDrawer: true,
             map: null,
-            end: 2017,
+            end: 2015,
             begin: 2000,
-            daterange: [2016, 2017],
-            timerange: [],
-            graphdata: {time: [], data: []},
-            hoverfeature: null,
+            dateRange: [2014, 2015],
+            timeRange: [],
+            graphData: {time: [], data: []},
+            hoverFeature: null,
             items: [
                 { title: 'home', icon: 'dashboard' },
                 { title: 'about', icon: 'question_answer' }
@@ -41,45 +41,50 @@ export default {
         }
     },
     mounted() {
-        this.gettimerange()
+        this.getTimeRange()
         // by default only load last year
-        this.$store.commit('requestyear', this.end)
+        this.$store.commit('requestYear', this.end)
         // now we can request to load  layer data
 
         this.$refs.timeslider.$on('time-extent-update', (event) => {
-            this.daterange = [
-                _.tointeger(event.from_pretty),
-                _.tointeger(event.to_pretty)
+            this.dateRange = [
+                _.toInteger(event.from_pretty),
+                _.toInteger(event.to_pretty)
             ]
-            this.setfilter()
+            this.setFilter()
         })
         this.map = this.$refs.map.map
         this.map.on('load', () => {
 
-            this.map.addsource("sdc-med-profiles", {
-                "url": "mapbox://siggyf.sdc-med-profiles",
-                "type": "vector"
-            })
-            _.foreach(sources, (source, id) => {
-                this.map.addsource(id,  source)
+            // this.map.addSource("sdc-med-profiles", {
+            //     "url": "mapbox://siggyf.sdc-med-profiles",
+            //     "type": "vector"
+            // })
+            _.forEach(sources, (source, id) => {
+                this.map.addSource(id,  source)
             })
             // add the hover layers
-            this.map.addsource('point-layer', {
-                "data": {type: 'featurecollection', features: []},
+            this.map.addSource('point-layer', {
+                "data": {type: 'FeatureCollection', features: []},
                 "type": "geojson"
             })
-            layers.foreach(layer => {
-                this.map.addlayer(layer)
+            layers.forEach(layer => {
+                this.map.addLayer(layer)
             })
 
             this.map.on('mousemove', (e) => {
-                let year = this.daterange[1]
-                let yearRange = _.range(this.daterange[0], this.daterange[1])
+                let year = this.dateRange[1]
+                let yearRange = _.range(this.dateRange[0], this.dateRange[1])
                 // set bbox as 5px reactangle area around clicked point
                 let buffer = 2
                 let bbox = [[e.point.x - buffer, e.point.y - buffer], [e.point.x + buffer, e.point.y + buffer]]
                 let features = this.map.queryRenderedFeatures(bbox, { layers: this.circleLayers })
+
+                // TODO: is  this needed?
+                //  features = JSON.parse(JSON.stringify(features))
+
                 this.map.getSource('point-layer').setData({type: 'FeatureCollection', features: features})
+                this.map.triggerRepaint()
                 if (features.length) {
                     this.hoverFeature = _.first(features)
                 } else {
@@ -97,11 +102,6 @@ export default {
             this.setFilter()
         })
     },
-    watch: {
-        layers () {
-            this.loadLayers()
-        }
-    },
     computed: {
         ...mapState([
             'layers',
@@ -115,11 +115,11 @@ export default {
             return circleLayers
         },
         heatmapLayers () {
-            let circleLayers = layers.filter(
-                (layer) => layer.type === 'circle' && layer['source-layer']
+            let heatmapLayers = layers.filter(
+                (layer) => layer.type === 'heatmap' && layer['source-layer']
             )
-            circleLayers = circleLayers.map(x => x.id)
-            return circleLayers
+            heatmapLayers = heatmapLayers.map(x => x.id)
+            return heatmapLayers
         }
 
     },
@@ -141,21 +141,11 @@ export default {
                     this.loadLayers()
                 })
         },
-        loadLayers () {
-            // loop over all layers and check if they're loaded. If not add  it.
-            _.each(this.layers, layer => {
-                if(!this.map.getSource(layer.id)){
-                    // add the layer
-                    this.map.addLayer(layer)
-                }
-            })
-
-        },
         setFilter () {
             let filter = [
                 'all',
-                ['>=', 'year', this.daterange[0]],
-                ['<=',  'year', this.daterange[1]]
+                ['>=', 'year', this.dateRange[0]],
+                ['<=',  'year', this.dateRange[1]]
             ]
             _.forEach(this.circleLayers, (layer) => {
                 this.map.setFilter(layer, filter)
