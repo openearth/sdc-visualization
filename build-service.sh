b07a38a483010e55a3c6a8bea5db1b90cf9b9841
Fedor Baart <fedor.baart@deltares.nl>
2020-02-10 09:59:55 +0200

    user container

--- build-service.sh
+++ build-service.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+datestr=$(date '+%Y-%m-%dT%H%M')
+echo building container build-${datestr}
 docker build -t sdc-visualization-service -f Dockerfile.sdc-visualization-service .
-docker tag sdc-visualization-service openearth/sdc-visualization-service
-docker push openearth/sdc-visualization-service
+
+# Push to dockerhub
+docker tag sdc-visualization-service openearth/sdc-visualization-service:build-${datestr}
+docker push openearth/sdc-visualization-service:build-${datestr}
+
+# Push to greece
+docker tag sdc-visualization-service registry-sdc.argo.grnet.gr/sdc-visualization-service:build-${datestr}
+docker push registry-sdc.argo.grnet.gr/sdc-visualization-static:build-${datestr}
