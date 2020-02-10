5936b7cab41c8d8f7a6b6a53137f69afe485cff5
Fedor Baart <fedor.baart@deltares.nl>
2020-02-10 16:45:25 +0200

    always build website

--- build-static.sh
+++ build-static.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 datestr=$(date '+%Y-%m-%dT%H%M')
 
+#  build the  webservice
+pushd sdc-static
+npm run build
+popd
+
 docker build -t sdc-visualization-static -f Dockerfile.sdc-visualization-static .
 
 # Push to dockerhub
