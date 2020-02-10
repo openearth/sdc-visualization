99bb6e80aa5ee64524c1e4d7a64469f2a5c011dc
Fedor Baart <fedor.baart@deltares.nl>
2020-02-10 12:43:15 +0200

    cleanup build scripts

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-docker build -t sdc-visualization-service -f Dockerfile.sdc-visualization-service .
-docker build -t sdc-visualization-static -f Dockerfile.sdc-visualization-static .
-docker build -t sdc-visualization-notebook -f Dockerfile.sdc-visualization-notebook .
-
-docker tag sdc-visualization-service openearth/sdc-visualization-service
-docker tag sdc-visualization-static openearth/sdc-visualization-static
-docker tag sdc-visualization-notebook openearth/sdc-visualization-notebook
-
-docker push openearth/sdc-visualization-service
-docker push openearth/sdc-visualization-static
-docker push openearth/sdc-visualization-notebook
-
-# docker tag sdc-visualization-service registry-sdc.argo.grnet.gr/sdc-visualization-service:devel
-# docker tag sdc-visualization-static registry-sdc.argo.grnet.gr/sdc-visualization-static:devel
-# docker tag sdc-visualization-notebook registry-sdc.argo.grnet.gr/sdc-visualization-notebook:devel
-# docker push registry-sdc.argo.grnet.gr/sdc-visualization-service:devel
-# docker push registry-sdc.argo.grnet.gr/sdc-visualization-static:devel
-# docker push registry-sdc.argo.grnet.gr/sdc-visualization-notebook:devel
+./build-static.sh
+./build-service.sh
--- build-service.sh
+++ build-service.sh
@@ -7,6 +7,14 @@ docker build -t sdc-visualization-service -f Dockerfile.sdc-visualization-servic
 docker tag sdc-visualization-service openearth/sdc-visualization-service:build-${datestr}
 docker push openearth/sdc-visualization-service:build-${datestr}
 
+# also latest
+docker tag sdc-visualization-service openearth/sdc-visualization-service:latest
+docker push openearth/sdc-visualization-service:latest
+
 # Push to greece
 docker tag sdc-visualization-service registry-sdc.argo.grnet.gr/sdc-visualization-service:build-${datestr}
-docker push registry-sdc.argo.grnet.gr/sdc-visualization-static:build-${datestr}
+docker push registry-sdc.argo.grnet.gr/sdc-visualization-service:build-${datestr}
+
+# also as latest
+docker tag sdc-visualization-service registry-sdc.argo.grnet.gr/sdc-visualization-service:latest
+docker push registry-sdc.argo.grnet.gr/sdc-visualization-service:latest
