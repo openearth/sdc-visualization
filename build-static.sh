6ce6f42d3bd5a18be9c77e985805bc6929174d94
Fedor Baart <fedor.baart@deltares.nl>
2020-04-14 18:05:03 +0200

    docs

--- build-static.sh
+++ build-static.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 datestr=$(date '+%Y-%m-%dT%H%M')
 
-#  build the  webservice
+# This assumes you have ran `npm install` in sdc-static
+
+# Build the webservice
 pushd sdc-static
 npm run build
 popd
 
+# Put it in a docker container
 docker build -t sdc-visualization-static -f Dockerfile.sdc-visualization-static .
